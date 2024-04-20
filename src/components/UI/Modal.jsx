bc292dcd87166d0fe18fb7b2e9be22b9c730322b
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-21 00:30:20 +0300

    Add reusable Modal component

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,31 @@
+import { useRef, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+import classes from './Modal.module.scss';
+
+function Modal({ open, onClose, children }) {
+  const modal = useRef();
+
+  useEffect(() => {
+    const dialog = modal.current;
+
+    if (open) {
+      dialog.showModal();
+    }
+
+    return () => dialog.close();
+  }, [open]);
+
+  const portalElement = document.getElementById('overlays');
+
+  return createPortal(
+    <dialog className={classes.modal} ref={modal} onClose={onClose}>
+      <div className={classes.modal__content}>
+        {children}
+      </div>
+    </dialog>,
+    portalElement
+  );
+}
+
+export default Modal;
