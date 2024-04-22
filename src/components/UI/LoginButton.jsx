6b69b95045e21f99b1679110de66f93b77711417
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-22 17:58:52 +0300

    Add LoginButton component (based on Button)

---
+++ src/components/UI/LoginButton.jsx
@@ -0,0 +1,24 @@
+import Button from './Button.jsx';
+import classes from './Button.module.scss';
+
+function LoginButton({ className = '' }) {
+  return (
+    <Button
+      className={className}
+      modifier='login'
+    >
+      <svg
+        className={`${classes.button__icon} ${classes['button__icon--login']}`}
+        width='16'
+        height='16'
+        aria-hidden='true'
+        focusable='false'
+      >
+        <use xlinkHref='#icon-login'></use>
+      </svg>
+      Login
+    </Button>
+  );
+}
+
+export default LoginButton;
