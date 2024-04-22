39ca76be2ba5997cf6248078684b4c35de43aa76
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-22 18:05:15 +0300

    Add MainNav component (using SiteList for it)

---
+++ src/components/Layout/MainNav/MainNav.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+import SiteList from './SiteList.jsx';
+import classes from './MainNav.module.scss';
+
+function MainNav({ className = '' }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const buttonSubClass = expanded ? classes['main-nav__toggle--open'] : '';
+  const menuId = 'site-list';
+
+  function handleToggleNav() {
+    setExpanded((prevExpanded) => !prevExpanded);
+  }
+
+  return (
+    <nav className={`${className} ${classes['main-nav']}`} aria-label='Primary'>
+      <button
+        className={`${classes['main-nav__toggle']} ${buttonSubClass}`}
+        type='button'
+        aria-controls={menuId}
+        aria-expanded={expanded}
+        aria-label='Open'
+        onClick={handleToggleNav}
+      />
+      <SiteList id={menuId} open={expanded} />
+    </nav>
+  );
+}
+
+export default MainNav;
