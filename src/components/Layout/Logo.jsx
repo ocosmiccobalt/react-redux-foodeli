f22c1680bbeb02a68383b2b80e847f3cad019f38
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-22 17:55:54 +0300

    Add Logo component & asset for it

---
+++ src/components/Layout/Logo.jsx
@@ -0,0 +1,18 @@
+import classes from './Logo.module.scss';
+
+function Logo({ className = '' }) {
+  return (
+    <a className={`${className} ${classes.logo}`} href='#'>
+      <img
+        className={classes.logo__image}
+        src='/img/logo-group.svg'
+        width='152'
+        height='45'
+        fetchPriority='high'
+        alt='Foodeli logo.'
+      />
+    </a>
+  );
+}
+
+export default Logo;
