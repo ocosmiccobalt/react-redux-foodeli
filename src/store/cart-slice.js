d003dc7ac0adb364e145ae80ef5a4543d5931b58
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-22 18:18:58 +0300

    Create Cart slice with @reduxjs/toolkit

---
+++ src/store/cart-slice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalPrice: 0,
+    changed: false
+  },
+  reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.totalPrice = action.payload.totalPrice;
+      state.items = action.payload.items;
+    },
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const newItemTotalPrice = newItem.price * newItem.quantity;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity = state.totalQuantity + newItem.quantity;
+      state.totalPrice = state.totalPrice + newItemTotalPrice;
+      state.changed = true;
+
+      if (!existingItem) {
+        state.items.push({
+          ...newItem, // { id: ..., title: ..., price: ..., quantity: ... }
+          totalPrice: newItemTotalPrice
+        });
+      } else {
+        existingItem.quantity = existingItem.quantity + newItem.quantity;
+        existingItem.totalPrice = existingItem.totalPrice + newItemTotalPrice;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      state.totalPrice = state.totalPrice - existingItem.price;
+      state.changed = true;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    }
+  }
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
