392fb12c42ef9a6f818806ecfcd1dbe2ffb29c88
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-22 17:59:49 +0300

    Add CartButton component (based on Button)

---
+++ src/components/Cart/CartButton.jsx
@@ -0,0 +1,27 @@
+import Button from '../UI/Button.jsx';
+import classes from '../UI/Button.module.scss';
+
+function CartButton({ className = '' }) {
+  return (
+    <Button
+      className={className}
+      modifier='cart'
+    >
+      <span className='visually-hidden'>Open the Cart:</span>
+      <span className={classes.button__amount}>
+        4
+      </span>
+      <svg
+        className={`${classes.button__icon} ${classes['button__icon--cart']}`}
+        width='20'
+        height='22'
+        aria-hidden='true'
+        focusable='false'
+      >
+        <use xlinkHref='#icon-cart'></use>
+      </svg>
+    </Button>
+  );
+}
+
+export default CartButton;
