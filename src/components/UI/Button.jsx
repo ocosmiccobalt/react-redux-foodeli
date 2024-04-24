f648d571d9db5b6c9b13ae5a497e91855365d272
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-25 01:36:35 +0300

    Update Button cmp to make it usable in tabbed UI

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,13 +1,35 @@
+import { forwardRef, useRef, useImperativeHandle } from 'react';
+
 import classes from './Button.module.scss';
 
-function Button({
-  className = '',
-  modifiers = [],
-  type,
-  onClick,
-  disabled,
-  children
-}) {
+const Button = forwardRef(function Button(
+  {
+    id,
+    index,
+    className = '',
+    modifiers = [],
+    type,
+    role, // e.g. role='tab'
+    ariaControls,
+    ariaSelected,
+    tabIndex,
+    onClick,
+    disabled,
+    children
+  },
+  ref
+) {
+  const buttonRef = useRef();
+
+  useImperativeHandle(ref, () => {
+    const button = buttonRef.current;
+
+    return {
+      focus: () => button.focus(),
+      blur: () => button.blur()
+    };
+  });
+
   const subClasses = modifiers.map((m) => classes['button--' + m]);
   const fullClassName = [
     className,
@@ -17,14 +39,21 @@ function Button({
 
   return (
     <button
+      id={id}
+      data-index={index}
       className={fullClassName}
       type={type || 'button'}
+      role={role}
+      aria-controls={ariaControls}
+      aria-selected={ariaSelected}
+      tabIndex={tabIndex}
       onClick={onClick}
       disabled={disabled}
+      ref={buttonRef}
     >
       {children}
     </button>
   );
-}
+});
 
 export default Button;
