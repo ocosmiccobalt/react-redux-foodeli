6c155e89560c8b9c693f8deb49c3901f979c7a92
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-04-27 01:06:18 +0300

    Use Cart component in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import PageHeader from './components/Layout/PageHeader.jsx';
 import Products from './components/Shop/Products.jsx';
+import Cart from './components/Cart/Cart.jsx';
 
 function App() {
   return (
     <>
+      <Cart />
       <PageHeader />
       <main>
         <h1>Claim Best Offer on Fast <i>Food</i> & <i>Restaurants</i></h1>
