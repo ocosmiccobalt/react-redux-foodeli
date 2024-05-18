2e0aad29cc1b60ac2369d5a9ca0be802f528d139
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-05-19 00:31:20 +0300

    Add PageMain cmp, use Intro & Services in it

---
+++ src/components/Layout/PageMain.jsx
@@ -0,0 +1,17 @@
+import Intro from './Intro.jsx';
+import Services from './Services/Services.jsx';
+import classes from './PageMain.module.scss';
+
+function PageMain({ children }) {
+  return (
+    <main
+      className={classes['page-main']}
+    >
+      <Intro id='why' />
+      <Services id='services' />
+      {children}
+    </main>
+  );
+}
+
+export default PageMain;
