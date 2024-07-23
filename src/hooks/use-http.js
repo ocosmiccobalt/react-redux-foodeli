260dbad06b74246d954bacccc8b789bca5c69049
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-07-24 00:01:49 +0300

    Import sendHttpRequest function to useHttp custom hook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,18 +1,6 @@
 import { useState, useCallback, useEffect } from 'react';
 
-async function sendHttpRequest(url, config) {
-  const response = await fetch(url, config);
-
-  const resData = await response.json();
-
-  if (!response.ok) {
-    throw new Error(
-      resData.message || 'Something went wrong, failed to send request.'
-    );
-  }
-
-  return resData;
-}
+import sendHttpRequest from '../util/send-http-request.js';
 
 function useHttp(url, config, initialData) {
   const [data, setData] = useState(initialData);
