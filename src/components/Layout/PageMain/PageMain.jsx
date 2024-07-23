b62b3e325b07ad657acb57562d3feab4259580e4
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-07-24 01:09:22 +0300

    Use Notification cmp & its piece of UI slice in PageMain cmp

--- src/components/Layout/PageMain/PageMain.jsx
+++ src/components/Layout/PageMain/PageMain.jsx
@@ -1,13 +1,25 @@
+import { useSelector } from 'react-redux';
+
 import Intro from '../Intro/Intro.jsx';
+import Notification from '../../UI/Notification/Notification.jsx';
 import Services from '../Services/Services.jsx';
 import classes from './PageMain.module.scss';
 
 function PageMain({ children }) {
+  const notification = useSelector((state) => state.ui.notification);
+
   return (
     <main
       className={classes['page-main']}
     >
       <Intro id='why' />
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Services id='services' />
       {children}
     </main>
